63bf265a1db2e20c5172d0fbe40be21c9d15f15f
Giovanni Masala <giovannimasala@Giovannis-MacBook-Pro.local>
2024-03-09 17:38:50 +0100

    added anew code snippet in useNavigate

--- useNavigate.js
+++ useNavigate.js
@@ -18,3 +18,23 @@ export const ExampleForm = () => {
   )
 }
 
+// The useNavigate() function also gives us the ability to programmatically navigate our users through their history stack. Scenarios like enabling users to go forward or backward in an application, or redirecting users to their previous page after they’ve logged in, are great use cases for this functionality. To navigate a user through their history stack using useNavigate(), we’d pass in an integer to indicate where in the history we’d like to travel. A positive integer navigates forward and a negative integer navigates backward.
+
+// For example:
+
+// navigate(-1) - navigate to the previous URL in the history stack.
+// navigate(1) - navigate to the next URL in the history stack.
+// navigate(-3) - navigate 3 URLs back in the history stack.
+// Below, we can see how the navigate() function is used to enable a “Go Back” button:
+
+import { useNavigate } from `react-router-dom`
+
+export const BackButton = () => {
+  const navigate = useNavigate()
+
+  return (
+    <button onClick={() => navigate(-1)}>
+      Go Back
+    </button>
+  )
+}
